fa17a8c0948159790b2ed68a2475e3f37fae9cc3
AlexMcClung <AlexMcClung@users.noreply.github.com>
2018-03-14 15:37:53 -0400

    Update and rename usn-class-size.r to cds-class-size.r

--- cds-class-size.r
+++ cds-class-size.r
@@ -1,3 +1,5 @@
+# Use this to create class section size bands for the Common Data Set
+
 df$SizeBand[df$ENROLLMENT<=9] <- '2-9' 
 df$SizeBand[df$ENROLLMENT>=10 & df$ENROLLMENT<=19] <- '10-19'
 df$SizeBand[df$ENROLLMENT>=20 & df$ENROLLMENT<=29] <- '20-29'
@@ -8,5 +10,4 @@ df$SizeBand[df$ENROLLMENT>=100] <- '100+'
 
 df$SizeBand <- factor(df$SizeBand, levels = c('2-9', '10-19', '20-29', '30-39', '40-49', '50-99', '100+'))
 
-# summary
 table(df$SizeBand, useNA = 'i')
