3fb039213e08cb308e6653096c0295a4f20fae54
AlexMcClung <AlexMcClung@users.noreply.github.com>
2018-03-14 18:46:29 -0400

    Update tidyverse.r

--- tidyverse.r
+++ tidyverse.r
@@ -4,7 +4,7 @@
 
 require('tidyverse')
 
-# Use list of IDs from a roster-type file to create variable in census
+## Use list of IDs from a roster-type file to create variable in census
 
 load('fall2017.RData') # Fall census file
 
@@ -13,4 +13,16 @@ i <- read_csv('sports-roster.csv') %>%
   filter(sport == 'badminton') %>%
   pull(ID)
   
-fall2017 <- fall2017 %>% mutate(badminton = ifelse(ID %in% i, 1, 0))
+fall2017 <- fall2017 %>% 
+  mutate(badminton = ifelse(ID %in% i, 1, 0))
+
+
+## Filtering joins can be very helpful
+
+s <- read_csv('sports-roster.csv') %>% 
+
+# Census for only those who participated in badminton
+fall2017 %>% semi_join(s, by = 'ID') # add to pipeline here
+
+# Census for non badminton people
+fall2017 %>% anti_join(s, by = 'ID') # add to pipeline here
