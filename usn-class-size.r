28b5e1eeeb8f5400150f0f88f6e6fbc703f933bc
AlexMcClung <AlexMcClung@users.noreply.github.com>
2018-03-14 15:36:36 -0400

    Create usn-class-size.r

---
+++ usn-class-size.r
@@ -0,0 +1,12 @@
+df$SizeBand[df$ENROLLMENT<=9] <- '2-9' 
+df$SizeBand[df$ENROLLMENT>=10 & df$ENROLLMENT<=19] <- '10-19'
+df$SizeBand[df$ENROLLMENT>=20 & df$ENROLLMENT<=29] <- '20-29'
+df$SizeBand[df$ENROLLMENT>=30 & df$ENROLLMENT<=39] <- '30-39'
+df$SizeBand[df$ENROLLMENT>=40 & df$ENROLLMENT<=49] <- '40-49'
+df$SizeBand[df$ENROLLMENT>=50 & df$ENROLLMENT<=99] <- '50-99'
+df$SizeBand[df$ENROLLMENT>=100] <- '100+'
+
+df$SizeBand <- factor(df$SizeBand, levels = c('2-9', '10-19', '20-29', '30-39', '40-49', '50-99', '100+'))
+
+# summary
+table(df$SizeBand, useNA = 'i')
